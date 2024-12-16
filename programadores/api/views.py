b3bb7e6fe0a113c729963612c57f3b9c28e72047
Uwowa <aurora.riquelme02@inacapmail.cl>
2024-12-16 01:15:30 -0300

    jwt incorporation

--- programadores/api/views.py
+++ programadores/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from programadores.models import Programmer
 from programadores.api.serializer import ProgrammerSerializer
 
 class ProgrammerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]  
     queryset = Programmer.objects.all()
     serializer_class = ProgrammerSerializer
--- programadores/awa/views.py
+++ programadores/awa/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from programadores.models import Programmer
-from app.awa.serializer import ProgrammerSerializer
+from programadores.api.serializer import ProgrammerSerializer
 
 class ProgrammerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Programmer.objects.all()
     serializer_class = ProgrammerSerializer
